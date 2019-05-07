0fa3051091fc7286682dae2664c0c767b2bb2d75
santsaxena <ags51197@gmail.com>
2019-05-07 16:27:27 -0700

    adding code

--- src/app/main-content/main-content.component.ts
+++ src/app/main-content/main-content.component.ts
@@ -1,5 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
+interface IStudent {
+  id: number;
+  firstName: string;
+  lastName: string;
+  course: string;
+  editMode: boolean;
+}
 @Component({
   selector: 'app-main-content',
   templateUrl: './main-content.component.html',
@@ -7,9 +14,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class MainContentComponent implements OnInit {
 
+
+  studentArray: Array<IStudent> = [];
+  disableAddButton = false;
   constructor() { }
 
   ngOnInit() {
+    this.studentArray = [
+      {
+        id: 1,
+        firstName: 'Sachin',
+        lastName: 'Tendulkar',
+        course: 'Cricket',
+        editMode: false
+      },
+      {
+        id: 2,
+        firstName: 'Lionel',
+        lastName: 'Messi',
+        course: 'Soccer',
+        editMode: false
+      },
+      {
+        id: 3,
+        firstName: 'Roger',
+        lastName: 'Federer',
+        course: 'Lawn Tennis',
+        editMode: false
+      },
+    ];
+  }
+
+
+  addStudent() {
+    this.studentArray.unshift({
+      id: null,
+      firstName: null,
+      lastName: null,
+      course: null,
+      editMode: true
+    });
+    this.disableAddButton = true;
+  }
+
+  removeStudent(index: number) {
+    this.studentArray.splice(index, 1);
+  }
+
+  saveStudent() {
+    this.studentArray[0].editMode = false;
+    this.disableAddButton = false;
+
+    this.sort('asc');
+  }
+
+  sort(direction: string) {
+    this.studentArray.sort((a: IStudent, b: IStudent) => {
+      return a.id < b.id ? -1 : 1;
+    });
   }
 
 }
